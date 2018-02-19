db3820a7aa0ad5527f0b60155d82c8a83a20e641
Mikhail-QA <Ponomarev_mi@mail.ru>
2018-02-20 00:03:39 +0300

    Добавил декораторы allure

--- Smoke/test_21_Проверяю_видео_в_плеере_Youtube.py
+++ Smoke/test_21_Проверяю_видео_в_плеере_Youtube.py
@@ -3,12 +3,9 @@ import time
 import unittest
 from POM.page_free_lesson import PageFreeLessonCheckVideo
 from POM.setUp import StartInterneturok
-from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.common.by import By
 
-@allure.story("Проверяю воспроизведения Плеера ИУ")
-@allure.severity("blocker")
 @allure.feature("Плеер Ютуб")
+@allure.story("Проверяю воспроизведения видео в плеере YouTube")
 class PlayVideoYouTube(StartInterneturok):
     def test_video_plays_youtube(self):
         driver = self.driver
@@ -17,22 +14,12 @@ class PlayVideoYouTube(StartInterneturok):
             driver.get(
                 "https://fast-staging.interneturok.ru/idei-i-smysly/osnovy-ratsionalnogo-povedeniya/spisok-urokov/kogda-nam-trudno-vybirat-paradoks-kondorse")
         time.sleep(5)
-        steps_video.click_play_video_YouTube()
-        time.sleep(2)
-
-        #     self.assertTrue(self.is_element_present(By.CSS_SELECTOR, "div.ytp-endscreen-content"))
-        #
-        # def is_element_present(self, how, what):
-        #     try:
-        #         self.driver.find_element(by=how, value=what)
-        #     except NoSuchElementException as e:
-        #         return False
-        #     return True
-        # self.assertTrue(driver.find_element_by_css_selector("div.ytp-upnext.ytp-upnext-autoplay-paused"))
+        with allure.step("Нажать на область превью"):
+            steps_video.click_play_video_YouTube()
 
     def tearDown(self):
         self.driver.quit()
 
 
 if __name__ == "__main__":
-   unittest.main()
+    unittest.main()
--- Smoke/test_22_Проверяю_видео_в_плеере_ИУ.py
+++ Smoke/test_22_Проверяю_видео_в_плеере_ИУ.py
@@ -1,13 +1,11 @@
 import unittest
 import allure
-
 from POM.page_free_lesson import PageFreeLessonCheckVideo
 from POM.setUp import StartInterneturok
 
 
-@allure.story("Проверяю воспроизведения Плеера ИУ")
-@allure.severity("blocker")
 @allure.feature("Плеер ИУ")
+@allure.story("Проверяю воспроизведения видео в плеере ИУ")
 class PlayVideoIu(StartInterneturok):
     def test_check_player_iu(self):
         driver = self.driver
@@ -17,7 +15,7 @@ class PlayVideoIu(StartInterneturok):
                 "https://fast-staging.interneturok.ru/geografy/6-klass/bgidrosferab/svoystva-vod-mirovogo-okeana")
         with allure.step("Нажать на кнопку Плей"):
             steps_video.click_play_video_iu()
-        with allure.step("Проверяю мой объект"):
+        with allure.step("После вкл видео в плеере появилась кнопка Pause"):
             steps_video.watching_that_video_iu_was_reproduced()
 
     def tearDown(self):
@@ -25,4 +23,4 @@ class PlayVideoIu(StartInterneturok):
 
 
 if __name__ == "__main__":
-   unittest.main()
+    unittest.main()
