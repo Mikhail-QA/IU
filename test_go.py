1c21b65e1b85cf2ce2e032a6621e6e01f7015d42
Mikhail-QA <Ponomarev_mi@mail.ru>
2018-02-19 15:43:48 +0300

    delete file

--- test_go.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from Smoke.test_21 import PlayVideoYouTube
-from Smoke.test_20 import PlayVideoIu
-
-
-suite = unittest.TestSuite()
-
-suite.addTest(unittest.makeSuite(PlayVideoIu))
-suite.addTest(unittest.makeSuite(PlayVideoYouTube))
