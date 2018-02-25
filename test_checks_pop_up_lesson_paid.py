e5c8f1bca10581aacd5b9389dedba1dd8bec744b
Mikhail-QA <Ponomarev_mi@mail.ru>
2018-02-26 01:08:50 +0300

    test_checks_pop_up_lesson_paid.py
    Добавил test-suite

--- test_checks_pop_up_lesson_paid.py
+++ test_checks_pop_up_lesson_paid.py
@@ -1,6 +1,13 @@
 import unittest
-from PopUp.test_1_Платный_урок_проверка_через_все_места import CheckAppearancePopUpInAllPlaces
+from Checks_PopUp_in_lesson_paid.User_not_auth.test_1_Платный_урок_проверка_через_все_места import \
+    CheckAppearancePopUpInAllPlaces
+from Checks_PopUp_in_lesson_paid.User_not_auth.test_2_Платный_урок_проверка_через_кнопку_Оплатить_абонемент import \
+    ClickButtonBuyTicketInPayLesson
+from Checks_PopUp_in_lesson_paid.User_not_auth.test_3_Платный_урок_проверка_через_кнопку_Войти import \
+    ClickButtonSignInPayLesson
 
 suite = unittest.TestSuite()
 
 suite.addTest(unittest.makeSuite(CheckAppearancePopUpInAllPlaces))
+suite.addTest(unittest.makeSuite(ClickButtonBuyTicketInPayLesson))
+suite.addTest(unittest.makeSuite(ClickButtonSignInPayLesson))
