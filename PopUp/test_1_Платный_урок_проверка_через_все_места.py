f0538652328082f60c6db4767600335a1a56af2a
Mikhail-QA <Ponomarev_mi@mail.ru>
2018-02-25 23:59:42 +0300

    Checks Pop-up in lesson paid\User not auth\test_1
    Добавил декораторы от allure

--- PopUp/test_1_Платный_урок_проверка_через_все_места.py
+++ PopUp/test_1_Платный_урок_проверка_через_все_места.py
@@ -1,3 +1,4 @@
+import allure
 from POM.setUp import StartInterneturokClassMethod
 from POM.page_paid_lesson import PaidLesson
 from POM.url_lesson import URLPaidLesson
@@ -7,6 +8,8 @@ from POM.page_paid_lesson import Value
 from POM.page_paid_lesson import PagePaidLessonComment
 
 
+@allure.feature("Поп-апы")
+@allure.story("Не авторизованным П проверяю в платном уроке появления поп-апов во всех местах кроме заглушки")
 class CheckAppearancePopUpInAllPlaces(StartInterneturokClassMethod):
 
     def test_click_button_play_in_stub_video(self):
@@ -14,19 +17,26 @@ class CheckAppearancePopUpInAllPlaces(StartInterneturokClassMethod):
         step_user = PaidLesson(driver)
         get_url = URLPaidLesson(driver)
 
-        get_url.go_algebra_8_grade_video()
-        step_user.click_button_play_video()
-        self.assertEquals(u"Зарегистрируйтесь",
-                          self.driver.find_element_by_css_selector("h5.popup-header__title").text)
+        with allure.step("Перейти на страницу урока во вкалдку Видеоурок"):
+            get_url.go_algebra_8_grade_video()
+        with allure.step("Нажать на кнопку Плей"):
+            step_user.click_button_play_video()
+        with allure.step("Поп-ап Авторизации Появился"):
+            self.assertEquals(u"Зарегистрируйтесь",
+                              self.driver.find_element_by_css_selector("h5.popup-header__title").text)
 
     def test_click_button_comment_in_tab_ask_question(self):
         driver = self.driver
         step_user = PaidLesson(driver)
         url_get = URLPaidLesson(driver)
 
-        url_get.go_algebra_8_grade_questions()
-        step_user.click_button_comments()
-        self.assertEquals(u"Зарегистрируйтесь", self.driver.find_element_by_css_selector("h5.popup-header__title").text)
+        with allure.step("Перейти на страницу урока во вкалдку Вопросы к уроку"):
+            url_get.go_algebra_8_grade_questions()
+        with allure.step("Нажать на кнопку Комментировать"):
+            step_user.click_button_comments()
+        with allure.step("Поп-ап Регистрации появился"):
+            self.assertEquals(u"Зарегистрируйтесь",
+                              self.driver.find_element_by_css_selector("h5.popup-header__title").text)
 
     def test_click_button_save_in_note(self):
         driver = self.driver
