8c0483b0d8f38d48eec45fcffdc755ebfb3e6cc7
Mikhail-QA <Ponomarev_mi@mail.ru>
2018-02-26 11:49:12 +0300

    test_checks_pop_up_lesson_paid
    Добавил в список test-suite 2 набора тестов
    suite.addTest(unittest.makeSuite(UserAuthCheckAppearancePopUpInAllPlaces))
    suite.addTest(unittest.makeSuite(UserAuthClickButtonBuyTicketInPayLesson))

--- test_checks_pop_up_lesson_paid.py
+++ test_checks_pop_up_lesson_paid.py
@@ -5,9 +5,15 @@ from Checks_PopUp_in_lesson_paid.User_not_auth.test_2_Платный_урок_п
     ClickButtonBuyTicketInPayLesson
 from Checks_PopUp_in_lesson_paid.User_not_auth.test_3_Платный_урок_проверка_через_кнопку_Войти import \
     ClickButtonSignInPayLesson
+from Checks_PopUp_in_lesson_paid.User_without_ticket_is_logged_in.test_1_Платный_урок_проверка_через_все_места import \
+    UserAuthCheckAppearancePopUpInAllPlaces
+from Checks_PopUp_in_lesson_paid.User_without_ticket_is_logged_in.test_2_Платный_урок__проверка_через_кнопку_Оплатить_абонемент import \
+    UserAuthClickButtonBuyTicketInPayLesson
 
 suite = unittest.TestSuite()
 
+suite.addTest(unittest.makeSuite(UserAuthCheckAppearancePopUpInAllPlaces))
+suite.addTest(unittest.makeSuite(UserAuthClickButtonBuyTicketInPayLesson))
 suite.addTest(unittest.makeSuite(CheckAppearancePopUpInAllPlaces))
 suite.addTest(unittest.makeSuite(ClickButtonBuyTicketInPayLesson))
 suite.addTest(unittest.makeSuite(ClickButtonSignInPayLesson))
