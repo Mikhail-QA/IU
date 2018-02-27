071f453b3b7f7f7dd7882db038aa5a9e05377eb8
Mikhail-QA <Ponomarev_mi@mail.ru>
2018-02-27 13:04:20 +0300

    Cheks_PopUp_on_page_abonement\User_not_auth\test_1_
    Создал тест для проверка не авторизованного пользователя проверка появления поп-ап Регистрации

---
+++ Cheks_PopUp_on_page_abonement/User_not_auth/test_1_Проверка_через_кнопку_Оплатить_абонемент.py
@@ -0,0 +1,35 @@
+import allure
+from POM.setUp import StartInterneturokClassMethod
+from POM.page_abonement import PageAbonement
+from POM.page_abonement import URLAbonement
+
+@allure.feature("Поп-апы для не авторизованного пользователя")
+@allure.story(
+    "Не авторизованным П проверяю на странице абонемент появления поп-апов при клике на кнопку Оплатить абонемент")
+class ClickButtonBuyTicketOnPageAbonement(StartInterneturokClassMethod):
+
+    def test_click_button_buy_ticket_in_header(self):
+        driver = self.driver
+        step_user = PageAbonement(driver)
+        url_get = URLAbonement(driver)
+
+        with allure.step("Перейти на страницу урока Абонемент"):
+            url_get.go_page_abonement()
+        with allure.step("Нажать на кнопку Оплатить абонемент"):
+            step_user.click_button_buy_ticket_in_header()
+        with allure.step("Поп-ап Регистрации появился"):
+            self.assertEquals(u"Зарегистрируйтесь",
+                              self.driver.find_element_by_css_selector("h5.popup-header__title").text)
+
+    def test_click_button_buy_ticket_in_footer(self):
+        driver = self.driver
+        step_user = PageAbonement(driver)
+        url_get = URLAbonement(driver)
+
+        with allure.step("Перейти на страницу урока Абонемент"):
+            url_get.go_page_abonement()
+        with allure.step("Нажать на кнопку Оплатить абонемент"):
+            step_user.click_button_buy_ticket_in_footer()
+        with allure.step("Поп-ап Регистрации появился"):
+            self.assertEquals(u"Зарегистрируйтесь",
+                              self.driver.find_element_by_css_selector("h5.popup-header__title").text)
