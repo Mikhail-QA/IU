c4dbddc44b640e30fec171882e0e4d90a59f9e48
Mikhail-QA <Ponomarev_mi@mail.ru>
2018-02-27 13:03:34 +0300

    POM\page_abonement.py
    Создал ПОМ файл страница абонемент с методами для неавторизованного пользователя

---
+++ POM/page_abonement.py
@@ -0,0 +1,19 @@
+import time
+
+class URLAbonement(object):
+    def __init__(self, driver):
+        self.driver = driver
+
+    def go_page_abonement(self):
+        self.driver.get("https://fast-staging.interneturok.ru/abonement")
+        time.sleep(1.5)
+
+class PageAbonement(object):
+    def __init__(self, driver):
+        self.driver = driver
+
+    def click_button_buy_ticket_in_header(self):
+        self.driver.find_element_by_xpath("//div[2]/div/main/div[2]/div[2]/div[1]/div[2]/a").click()
+
+    def click_button_buy_ticket_in_footer(self):
+        self.driver.find_element_by_xpath("//div[2]/div/main/div[2]/div[4]/div/a").click()
