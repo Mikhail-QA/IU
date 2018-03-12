8e7f406071a8ea1ebeea589715f7ee31bd40189b
Mikhail-QA <Ponomarev_mi@mail.ru>
2018-03-12 10:50:07 +0300

    Web_services\Subject_page\Сheck that all elements of the grades page is displayed
    Добавил allure декораторы

--- Web_services/Subject_page/Сheck that all elements of the grades page is displayed/test_пользователь_авторизован_проверка_Header.py
+++ Web_services/Subject_page/Сheck that all elements of the grades page is displayed/test_пользователь_авторизован_проверка_Header.py
@@ -4,41 +4,51 @@
 URL: https://web-dev01.interneturok.ru/algebra/8-klass"
 На странице отображаются: http://joxi.ru/82Q0wVOH13jPlm
 """
+import allure
 from selenium.webdriver.common.by import By
 from Web_services.app.SetUp import StartInterneturokClassMethod
 from POM.popup_authorization_and_registration import PopupSignIn
 from POM.user import AutopaymentMailRu
 from POM.main_page import MainPage
+from Web_services.Subject_page.URL import UrlLesson
 
 
+@allure.feature("Страница Предмет-Класс (Алгебра 8 класс)")
+@allure.story("Проверка наличия элементов в Header для авторизованного пользователя")
 class ChecksAllElementsTheHeadersUserNotAuth(StartInterneturokClassMethod):
-    def test_001_go(self):
+    @allure.step("Авторизоваться пользователем")
+    def test_000_logged_user(self):
         StartInterneturokClassMethod = self.driver
         steps_main = MainPage(StartInterneturokClassMethod)
         steps_user = AutopaymentMailRu(StartInterneturokClassMethod)
         steps_popup = PopupSignIn(StartInterneturokClassMethod)
+        go_page = UrlLesson(StartInterneturokClassMethod)
         steps_main.go_to_sgnIn()
         steps_user.enter_email()
         steps_user.enter_password()
         steps_popup.click_button_login()
+        go_page.go_algebra_8_grade()
 
-    def test_002_open_page(self):
-        self.driver.get("https://fast-staging.interneturok.ru/algebra/8-klass")
-
-    def test_logo_interneturok(self):
+    @allure.step("Элемент Логотип отображается")
+    def test_logo_is_displayed(self):
         self.assertTrue(self.is_element_present(By.CSS_SELECTOR, "a.logo.header__logo"))
 
+    @allure.step("Кнопка Предметы отображается")
     def test_button_subjects_displayed(self):
         self.assertTrue(self.is_element_present(By.XPATH, "//header/div[1]/div[2]"))
 
+    @allure.step("Кнопка Классы отображается")
     def test_button_grades_displayed(self):
         self.assertTrue(self.is_element_present(By.XPATH, "//header/div[1]/div[1]"))
 
+    @allure.step("Элемент Флешка отображается")
     def test_field_search(self):
-        self.assertTrue(self.is_element_present(By.CSS_SELECTOR, "div.header-search__wraps"))
+        self.assertTrue(self.is_element_present(By.NAME, "query"))
 
-    def test_flash(self):
-        self.assertTrue(self.is_element_present(By.CSS_SELECTOR, "div.icon-flash"))
+    @allure.step("Кнопка Флешка отображается")
+    def test_flash_displayed(self):
+        self.assertTrue(self.is_element_present(By.CSS_SELECTOR, "div.icon.icon-flash"))
 
-    def test_signIn(self):
+    @allure.step("Кнопка Мой профиль отображается")
+    def test_signIn_is_displayed(self):
         self.assertEqual(u"Мой профиль", self.driver.find_element_by_css_selector("div.header__menu_profile").text)
--- Web_services/Subject_page/Сheck that all elements of the grades page is displayed/test_пользователь_не_авторизован_проверка_Body.py
+++ Web_services/Subject_page/Сheck that all elements of the grades page is displayed/test_пользователь_не_авторизован_проверка_Body.py
@@ -5,58 +5,88 @@ URL: https://web-dev01.interneturok.ru/algebra/8-klass"
 На странице отображаются: http://joxi.ru/vAWbBDLHkK1YM2
 """
 
+import allure
 from selenium.webdriver.common.by import By
 from Web_services.app.SetUp import StartInterneturokClassMethod
+from Web_services.Subject_page.URL import UrlLesson
 
 
+@allure.feature("Страница Предмет-Класс (Алгебра 8 класс)")
+@allure.story("Проверка наличия элементов в Body для не авторизованного пользователя")
 class ChecksAllElementsTheBodyUserNotAuth(StartInterneturokClassMethod):
-    def test_001_open_page(self):
-        self.driver.get("https://fast-staging.interneturok.ru/algebra/8-klass")
+    @allure.step("Авторизоваться пользователем")
+    def test_000_open_page(self):
+        StartInterneturokClassMethod = self.driver
+        go_page = UrlLesson(StartInterneturokClassMethod)
+        go_page.go_algebra_8_grade()
 
+    @allure.step("Отображается заголовок (Алгебра)")
     def test_title_body(self):
         self.assertEqual(u"Алгебра", self.driver.find_element_by_css_selector("h1.subject-title").text)
 
+    @allure.step("Отображается заголовок (8 класс)")
     def test_number_grades(self):
         self.assertEqual(u"8 класс", self.driver.find_element_by_css_selector("span.subject-nav__grade").text)
 
-    def test_lesson__notice_text(self):
-        self.assertTrue(self.driver.find_element_by_css_selector("a.lesson-paid.ember-view"))
-
-    # def test_lesson__noti2ce_text(self):
-    #     self.assertIn("Для просмотра уроков необходим абонемент",
-    #                   self.driver.find_element_by_css_selector("main.content.static").text)
-
-    def test_tooltip_lesson(self):
+    @allure.step("Отображается иконка (Звезда платный урок)")
+    def test_abonement_widget_displayed(self):
         self.assertTrue(self.driver.find_element_by_css_selector("a.lesson-paid"))
 
+    @allure.step("Отображается кнопка перехода на 7 класс")
     def test_switcher_link_left(self):
         self.assertTrue(self.driver.find_element_by_css_selector("a.subject-nav_prev"))
 
+    @allure.step("Отображается кнопка перехода на 9 класс")
     def test_switcher_link_right(self):
         self.assertTrue(self.driver.find_element_by_css_selector("a.subject-nav_next"))
 
+    @allure.step("Над учебниками отображается текст (Смотреть уроки как в учебнике:)")
     def test_text_name_books(self):
         self.assertEqual(u"Смотреть уроки как в учебнике:",
                          self.driver.find_element_by_class_name("subject-book__title").text)
 
-    def test_books_item(self):
+    @allure.step("На странице отображается книга (Алгебра 8 класс (Мордкович А.Г.))")
+    def test_books_item_one(self):
         self.assertTrue(self.driver.find_element_by_xpath("//div[1]/div[2]/div/div/a[1]"))
 
+    @allure.step("На странице отображается книга (Алгебра 8 класс (Макарычев Ю.Н.))")
     def test_books_item_two(self):
         self.assertTrue(self.driver.find_element_by_xpath("//div[1]/div[2]/div/div/a[2]"))
 
+    @allure.step(
+        "Первый блок уроков имеет заголовок (Алгебраические дроби. Арифметические операции над алгебраическими дробями) ")
     def test_zagolovok_temi_yroka(self):
         self.assertEqual(u"Алгебраические дроби. Арифметические операции над алгебраическими дробями",
                          self.driver.find_element_by_xpath("//div/ul/li[1]/h4").text)
 
+    @allure.step("Отображается список уроков")
     def test_list__item(self):
         self.assertTrue(self.driver.find_element_by_class_name("subject-theme__sublist"))
 
-    def test_ikonki_vkladok_urokov(self):
+    @allure.step("Отображается вкладки урока")
+    def test_icon_tab_grades(self):
         self.assertTrue(self.driver.find_element_by_class_name("subject-theme__icons"))
 
+    @allure.step("Отображается вкладка урока (Видеоурок)")
+    def test_mini_icon_video_displayed(self):
+        self.assertTrue(self.driver.find_element_by_class_name("icon-video"))
+
+    @allure.step("Отображается вкладка урока (Текстовый урок)")
+    def test_mini_icon_text_displayed(self):
+        self.assertTrue(self.driver.find_element_by_class_name("icon-text"))
+
+    @allure.step("Отображается вкладка урока (Тренажеры)")
+    def test_mini_icon_trainers_displayed(self):
+        self.assertTrue(self.driver.find_element_by_class_name("icon-trainers"))
+
+    @allure.step("Отображается вкладка урока (Тесты)")
+    def test_mini_icon_test_displayed(self):
+        self.assertTrue(self.driver.find_element_by_class_name("icon-test"))
+
+    @allure.step("Отображается список тем урока")
     def test_levyai_list_yrokov(self):
         self.assertTrue(self.driver.find_element_by_css_selector("ul.nav.menu__list"))
 
+    @allure.step("Отображается кнопка (Оставить отзыв)")
     def test_button_review_displayed(self):
         self.assertTrue(self.is_element_present(By.CSS_SELECTOR, "div.review__close"))
