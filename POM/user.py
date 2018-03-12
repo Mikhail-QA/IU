3c457eb716f5991721de801049f35fbef9d531f7
Mikhail-QA <Ponomarev_mi@mail.ru>
2018-03-12 08:48:02 +0300

    POM\user
    Удалил пробелмы в коде

--- POM/user.py
+++ POM/user.py
@@ -1,10 +1,4 @@
 # authorization user not abonement
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
-
 class AutopaymentMailRu(object):
     def __init__(self, driver):
         self.driver = driver
