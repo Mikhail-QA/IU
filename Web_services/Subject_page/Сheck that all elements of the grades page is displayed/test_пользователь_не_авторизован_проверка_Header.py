5fdf2ed9c943aaee27a9eec42f0fd586753606f3
Mikhail-QA <Ponomarev_mi@mail.ru>
2018-03-12 11:01:48 +0300

    Web_services\Subject_page\Сheck that all elements of the grades page is displayed
    Добавил allure декораторы

--- Web_services/Subject_page/Сheck that all elements of the grades page is displayed/test_пользователь_не_авторизован_проверка_Header.py
+++ Web_services/Subject_page/Сheck that all elements of the grades page is displayed/test_пользователь_не_авторизован_проверка_Header.py
@@ -5,28 +5,41 @@ URL: https://web-dev01.interneturok.ru/algebra/8-klass"
 На странице отображаются: http://joxi.ru/vAWbBDLHkK1YM2
 """
 
+import allure
 from selenium.webdriver.common.by import By
+from Web_services.Subject_page.URL import UrlLesson
 from Web_services.app.SetUp import StartInterneturokClassMethod
 
 
+@allure.feature("Страница Предмет-Класс (Алгебра 8 класс)")
+@allure.story("Проверка наличия элементов в Header для не авторизованного пользователя")
 class ChecksAllElementsTheHeadersUserNotAuth(StartInterneturokClassMethod):
-    def test_001_open_page(self):
-        self.driver.get("https://fast-staging.interneturok.ru/algebra/8-klass")
+    @allure.step("Авторизоваться пользователем")
+    def test_000_open_page(self):
+        StartInterneturokClassMethod = self.driver
+        go_page = UrlLesson(StartInterneturokClassMethod)
+        go_page.go_algebra_8_grade()
 
+    @allure.step("Элемент Логотип отображается")
     def test_logo_interneturok(self):
         self.assertTrue(self.is_element_present(By.CSS_SELECTOR, "a.logo.header__logo"))
 
+    @allure.step("Кнопка Предметы отображается")
     def test_button_subjects_displayed(self):
         self.assertTrue(self.is_element_present(By.XPATH, "//header/div[1]/div[2]"))
 
+    @allure.step("Кнопка Классы отображается")
     def test_button_grades_displayed(self):
         self.assertTrue(self.is_element_present(By.XPATH, "//header/div[1]/div[1]"))
 
+    @allure.step("Элемент Поиск отображается")
     def test_field_search(self):
         self.assertTrue(self.is_element_present(By.CSS_SELECTOR, "div.header-search__wraps"))
-    
-    def test_flash(self):
+
+    @allure.step("Кнопка Флешка отображается")
+    def test_button_flash(self):
         self.assertTrue(self.is_element_present(By.CSS_SELECTOR, "div.icon-flash"))
 
-    def test_signIn(self):
+    @allure.step("Кнопка Войти отображается")
+    def test_button_enter_is_displayed(self):
         self.assertEqual(u"Войти", self.driver.find_element_by_css_selector("span.button_login").text)
