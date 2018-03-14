d614b23c3c17c69bfa90d5958a1b2194365ede94
Mikhail-QA <Ponomarev_mi@mail.ru>
2018-03-14 15:38:13 +0300

    Smoke\test_000_Удаление_юзеров_в_админке
    Удаления П в админке, вынес в каждый Метод

--- Smoke/test_000_Удаление_юзеров_в_админке.py
+++ Smoke/test_000_Удаление_юзеров_в_админке.py
@@ -9,7 +9,7 @@ from POM.setUp import StartInterneturokAdmin
 @allure.feature("Админка")
 @allure.story("Удаляю пользователей в админке")
 class RemovingUsersInAdminPanel(StartInterneturokAdmin):
-    def test_removing_a_users(self):
+    def test_login_admin(self):
         driver = self.driver
         steps_admin = Admin(driver)
         steps_login = PopupSignIn(driver)
@@ -20,14 +20,31 @@ class RemovingUsersInAdminPanel(StartInterneturokAdmin):
             steps_login.click_button_login_in_admin_panel()
             steps_delete.go_to_admin()
 
+    def test_delete_vratchglavyandex(self):
+        driver = self.driver
+        steps_delete = AdminDeleteUser(driver)
+
         with allure.step("Удаляю пользователя vratch.glav@yandex.ru"):
             steps_delete.user_1(pupil_1="vratch.glav@yandex.ru")
+
+    def test_delete_paymnotyandex(self):
+        driver = self.driver
+        steps_delete = AdminDeleteUser(driver)
 
         with allure.step("Удаляю пользователя paym.not@yandex.ru"):
             steps_delete.user_2(pupil_2="paym.not@yandex.ru")
 
+    def test_delete_paymentnotmail(self):
+        driver = self.driver
+        steps_delete = AdminDeleteUser(driver)
+
         with allure.step("Удаляю пользователя payment.not@mail.ru"):
             steps_delete.user_3(pupil_3="payment.not@mail.ru")
+
+    def test_delete_iuuseryopmail(self):
+        driver = self.driver
+        steps_delete = AdminDeleteUser(driver)
+
         with allure.step("Удаляю пользователя iuuser@yopmail.com"):
             steps_delete.user_4(pupil_4="iuuser@yopmail.com")
 
