05ce861a4fce888df89429666841e8463db91fb6
Mikhail-QA <Ponomarev_mi@mail.ru>
2018-03-14 08:59:20 +0300

    Smoke\test_28_Проверка_перехода_по_ссылке_с_www
    Создал новый тест

---
+++ Smoke/test_28_Проверка_перехода_по_ссылке_с_www.py
@@ -0,0 +1,20 @@
+"""
+3. Проверка перехода на https://www.interneturok.ru/ должен открываться без "WWW" https://interneturok.ru/
+"""
+
+import time
+import allure
+from POM.setUp import StartInterneturokClassMethod
+
+
+@allure.feature("Проверка перехода по ссылке с (WWW)")
+@allure.story("Перейти по ссылке https://www.fast-staging.interneturok.ru/ и убедится что в URL не отображается WWW")
+class CheckCanonical(StartInterneturokClassMethod):
+    def test_attribute_canonical_is_HTTPS(self):
+        URL = "https://www.interneturok.ru"
+        driver = self.driver
+        driver.get(URL)
+        time.sleep(2)
+        with allure.step(
+                "После перехода по ссылке с (www.interneturok) П открывается ссылка без WWW (https://interneturok.ru/) "):
+            assert driver.current_url == "https://interneturok.ru/"
