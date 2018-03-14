08db14994da94d2e9473bce7fa6d5bb63667057c
Mikhail-QA <Ponomarev_mi@mail.ru>
2018-03-14 08:58:40 +0300

    Smoke\test_26_Проверка_title_keywords_description
    Создал новые тесты

---
+++ Smoke/test_26_Проверка_title_keywords_description.py
@@ -0,0 +1,86 @@
+"""
+1. Мета-теги / Title, Keywords, Description должны присутствовать и не должны быть пустыми + соответствовать страницы на которой находится П
+Главная
+Класс-Предмет
+Страница урока
+любая статичная страница. Например https://fast-staging.interneturok.ru/article/how-you-can-use-interneturok
+
+"""
+
+import allure
+import time
+from POM.setUp import StartInterneturokClassMethod
+
+
+@allure.feature("Проверка title, description, keywords на разных страницах")
+@allure.story("Перейти по страницам и сравнить title, description, keywords")
+class CheckMetaTeg(StartInterneturokClassMethod):
+    @allure.step("Проверка title на Главной странице")
+    def test_meta_teg_in_home_page(self):
+        URL = "https://fast-staging.interneturok.ru/"
+        driver = self.driver
+        with allure.step("Сверить url со ссылкой на которой находится П"):
+            assert driver.current_url == URL
+        with allure.step("Проверить соответсвия (title)"):
+            assert driver.title == "Видеоуроки школьной программы, конспекты, тесты, тренажеры"
+        with allure.step("Проверка соответствия (description)"):
+            self.assertEqual(u"Видеоуроки школьной программы, конспекты, тесты, тренажеры",
+                             driver.find_element_by_name("description").get_attribute('content'))
+        with allure.step("Проверка соответствия (Keywords)"):
+            self.assertEqual(
+                u"видео уроки, видеоуроки, смотреть видео, дистанционное образование, конспекты уроков, электронный учебник, школа-экстернат, помощь в учёбе, удалённая школа",
+                driver.find_element_by_name("keywords").get_attribute('content'))
+
+    @allure.step("Проверка title на странице урока")
+    def test_meta_teg_in_page_lesson(self):
+        driver = self.driver
+        URL = "https://fast-staging.interneturok.ru/physics/8-klass/teplovye-yavleniya/teplovoe-dvizhenie-temperatura"
+        driver.get(URL)
+        time.sleep(2)
+        with allure.step("Сверить url со ссылкой на которой находится П"):
+            assert driver.current_url == URL
+        with allure.step("Проверить соответсвия Title"):
+            assert driver.title == "Тепловое движение. Температура. Видеоурок. Физика 8 Класс"
+        with allure.step("Проверка соответствия (description)"):
+            self.assertEqual(u"Видеоурок: Тепловое движение. Температура по предмету Физика за 8 класс.",
+                             driver.find_element_by_name("description").get_attribute('content'))
+        with allure.step("Проверка соответствия (Keywords)"):
+            self.assertEqual(
+                u"Видеоурок Физика, видеоурок Физика 8 класс, Тепловое движение. Температура",
+                driver.find_element_by_name("keywords").get_attribute('content'))
+
+    @allure.step("Проверка title на странице Предмет-Класс ")
+    def test_meta_teg_in_page_subject_grades(self):
+        URL = "https://fast-staging.interneturok.ru/literatura/8-klass"
+        driver = self.driver
+        driver.get(URL)
+        time.sleep(2)
+        with allure.step("Сверить url со ссылкой на которой находится П"):
+            assert driver.current_url == URL
+        with allure.step("Проверить соответсвия Title"):
+            assert driver.title == "Литература 8 класс"
+        with allure.step("Проверка соответствия (description)"):
+            self.assertEqual(
+                u"Видеоуроки, тесты и тренажёры по Литература за 8 класс по школьной программе. Используйте конспект уроков раздела «Литература 8 класс» для закрепления полученных знаний.",
+                driver.find_element_by_name("description").get_attribute('content'))
+        with allure.step("Проверка соответствия (Keywords)"):
+            self.assertEqual(u"Литература 8 класс",
+                             driver.find_element_by_name("keywords").get_attribute('content'))
+
+    @allure.step("Проверка title на статичной странице")
+    def test_meta_teg_in_static_page(self):
+        URL = "https://fast-staging.interneturok.ru/article/how-you-can-use-interneturok"
+        driver = self.driver
+        driver.get(URL)
+        time.sleep(2)
+        with allure.step("Сверить url со ссылкой на которой находится П"):
+            assert driver.current_url == URL
+        with allure.step("Проверить соответсвия Title"):
+            assert driver.title == "Как использовать сайт InternetUrok.ru в школе?"
+
+        with allure.step("Проверка соответствия (description)"):
+            self.assertEqual(u"Как использовать сайт InternetUrok.ru в школе?",
+                             driver.find_element_by_name("description").get_attribute('content'))
+        with allure.step("Проверка соответствия (Keywords)"):
+            self.assertEqual(u"Как использовать сайт InternetUrok.ru в школе?",
+                             driver.find_element_by_name("keywords").get_attribute('content'))
