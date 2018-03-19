9846230a0e6faf75d0406718de36d5493bca8290
Mikhail-QA <Ponomarev_mi@mail.ru>
2018-03-19 10:54:00 +0300

    Smoke\test_10_Авторизоваться_без_абонемента_проверить_в_платном_уроке_видеоурок
    Добавил URL на страницу урока

--- Smoke/test_10_Авторизоваться_без_абонемента_проверить_в_платном_уроке_видеоурок.py
+++ Smoke/test_10_Авторизоваться_без_абонемента_проверить_в_платном_уроке_видеоурок.py
@@ -6,6 +6,7 @@ from POM.main_page import MainPage
 from POM.popup_authorization_and_registration import PopupSignIn
 from POM.setUp import StartInterneturok
 from POM.user import AutopaymentMailRu
+from POM.url_lesson import URLPaidLesson
 
 
 @allure.feature("Платный видеоурок")
@@ -16,6 +17,7 @@ class CheckNoSubscriptionVideoInPayLesson(StartInterneturok):
         main_steps = MainPage(driver)
         popup_steps = PopupSignIn(driver)
         user_steps = AutopaymentMailRu(driver)
+        get_url = URLPaidLesson(driver)
         with allure.step("Нажать на кнопку Войти"):
             main_steps.go_to_sgnIn()
         with allure.step("Ввожу email/password"):
@@ -24,8 +26,7 @@ class CheckNoSubscriptionVideoInPayLesson(StartInterneturok):
         with allure.step("Нажать на кнопку Авторизоваться"):
             popup_steps.click_button_login()
         with allure.step("Перейти на урок"):
-            self.driver.get(
-                "https://fast-staging.interneturok.ru/russian/9-klass/vvedenie/mezhdunarodnoe-znachenie-russkogo-yazyka")
+            get_url.go_algebra_8_grade_video()
         with allure.step("Проверить присутствие заглушки видеоурока"):
             self.assertTrue(self.is_element_present(By.CSS_SELECTOR, "div.video-blocker"))
 
