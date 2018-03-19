a2456953aa3c1d03874ac8154b6b54855d4ad66b
Mikhail-QA <Ponomarev_mi@mail.ru>
2018-03-19 11:19:06 +0300

    Smoke\test_15_Пройти_тест_в_бесплатном_уроке
    Поменял шаг перехода на страницу урока

--- Smoke/test_15_Пройти_тест_в_бесплатном_уроке.py
+++ Smoke/test_15_Пройти_тест_в_бесплатном_уроке.py
@@ -1,11 +1,11 @@
 import allure
-import time
 import unittest
 from POM.setUp import StartInterneturok
 from POM.main_page import MainPage
 from POM.user import PaymNotYandexRu
 from POM.popup_authorization_and_registration import PopupSignIn
 from POM.freetest import FreeTest
+from POM.url_lesson import URLFreeLesson
 
 
 @allure.feature("Тест")
@@ -17,6 +17,7 @@ class PassTestInFreeLesson(StartInterneturok):
         user_steps = PaymNotYandexRu(driver)
         popup_steps = PopupSignIn(driver)
         steps_test = FreeTest(driver)
+        get_url = URLFreeLesson(driver)
         with allure.step("Нажать на кнопку Войти"):
             main_steps.go_to_sgnIn()
         with allure.step("Ввожу email/password"):
@@ -25,9 +26,7 @@ class PassTestInFreeLesson(StartInterneturok):
         with allure.step("Нажать на кнопку Авторизоваться"):
             popup_steps.click_button_login()
         with allure.step("Перейти на урок"):
-            driver.get(
-                "https://fast-staging.interneturok.ru/biology/7-klass/zhivotnye-ploskie-chervi/lentochnye-chervi/testcases")
-        time.sleep(2)
+            get_url.go_biology_7_grade_test()
         with allure.step("Нажать на кнопку Пройти"):
             steps_test.go_test()
         with allure.step("Отвечать на вопросы в тесте"):
