7371a51d88c4c0664b58c660996f636ad7e06126
Mikhail-QA <Ponomarev_mi@mail.ru>
2018-03-19 11:23:29 +0300

    Smoke\test_17_Оставить_заметки_в_платном_уроке
    Поменял шаг перехода на страницу урока

--- Smoke/test_17_Оставить_заметки_в_платном_уроке.py
+++ Smoke/test_17_Оставить_заметки_в_платном_уроке.py
@@ -5,6 +5,7 @@ from POM.main_page import MainPage
 from POM.popup_authorization_and_registration import PopupSignIn
 from POM.user import PaymNotYandexRu
 from POM.page_paid_lesson import Notes
+from POM.url_lesson import URLPaidLesson
 
 
 @allure.feature("Заметка")
@@ -16,6 +17,7 @@ class WriteNoteInPayLesson(StartInterneturok):
         user_steps = PaymNotYandexRu(driver)
         popup_steps = PopupSignIn(driver)
         notes_steps = Notes(driver)
+        get_url = URLPaidLesson(driver)
         with allure.step("Нажать на кнопку Войти"):
             main_steps.go_to_sgnIn()
         with allure.step("Ввожу email/password"):
@@ -24,8 +26,7 @@ class WriteNoteInPayLesson(StartInterneturok):
         with allure.step("Нажать на кнопку Авторизоваться"):
             popup_steps.click_button_login()
         with allure.step("Перейти на урок"):
-            self.driver.get(
-                "https://fast-staging.interneturok.ru/physics/7-klass/rabota-moshnost-energija/energiya-zakon-sohraneniya-energii")
+            get_url.go_algebra_8_grade_video()
         with allure.step("На странице урока нажать на виджет Заметка"):
             notes_steps.open_notes()
         with allure.step("В поле ввода ввести текст Hello"):
