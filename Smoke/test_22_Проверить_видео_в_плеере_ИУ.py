101024daae269f4fd22383d3b7aed6d25be69bdd
Mikhail-QA <Ponomarev_mi@mail.ru>
2018-03-19 11:33:58 +0300

    Smoke\test_22_Проверить_видео_в_плеере
    Поменял шаг перехода на страницу урока

--- Smoke/test_22_Проверить_видео_в_плеере_ИУ.py
+++ Smoke/test_22_Проверить_видео_в_плеере_ИУ.py
@@ -2,6 +2,7 @@ import unittest
 import allure
 from POM.page_free_lesson import PageFreeLessonCheckVideo
 from POM.setUp import StartInterneturok
+from POM.url_lesson import URLFreeLesson
 
 
 @allure.feature("Плеер ИУ")
@@ -10,9 +11,9 @@ class PlayVideoIu(StartInterneturok):
     def test_check_player_iu(self):
         driver = self.driver
         steps_video = PageFreeLessonCheckVideo(driver)
+        get_url = URLFreeLesson(driver)
         with allure.step("Перейти на урок"):
-            driver.get(
-                "https://fast-staging.interneturok.ru/geografy/6-klass/bgidrosferab/svoystva-vod-mirovogo-okeana")
+            get_url.go_biology_11_grade_video()
         with allure.step("Нажать на кнопку Плей"):
             steps_video.click_play_video_iu()
         with allure.step("После вкл видео в плеере появилась кнопка Pause"):
