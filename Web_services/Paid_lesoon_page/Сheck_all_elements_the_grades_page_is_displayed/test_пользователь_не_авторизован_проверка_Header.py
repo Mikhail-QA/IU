db2b2dc4f7e8a06a7c8a7d416d78a7a674086b83
Mikhail-QA <Ponomarev_mi@mail.ru>
2018-03-19 17:43:34 +0300

    Web_services\Paid_lesoon_page\Сheck_all_elements_the_grades_page_is_displayed\test_пользователь_не_авторизован_проверка_Header
    Создал новый файл тестов

---
+++ Web_services/Paid_lesoon_page/Сheck_all_elements_the_grades_page_is_displayed/test_пользователь_не_авторизован_проверка_Header.py
@@ -0,0 +1,38 @@
+import allure
+from selenium.webdriver.common.by import By
+from Web_services.URL import Paid_lesson_page
+from Web_services.app.SetUp import StartInterneturokClassMethod
+
+
+@allure.feature("Страница урока Основные понятия (Алгебра 8 класс)")
+@allure.story("Проверка наличия элементов в Header для не авторизованного пользователя")
+class ChecksAllElementsInSubjectPageTheHeadersUserNotAuth(StartInterneturokClassMethod):
+    @allure.step("Перейти на страницу Алгебра 8 класс")
+    def test_000_open_page(self):
+        StartInterneturokClassMethod = self.driver
+        go_page = Paid_lesson_page(StartInterneturokClassMethod)
+        go_page.go_lesson_page()
+
+    @allure.step("Элемент Логотип отображается")
+    def test_logo_interneturok(self):
+        self.assertTrue(self.is_element_present(By.CSS_SELECTOR, "a.logo.header__logo"))
+
+    @allure.step("Кнопка Предметы отображается")
+    def test_button_subjects_displayed(self):
+        self.assertTrue(self.is_element_present(By.XPATH, "//header/div[1]/div[2]"))
+
+    @allure.step("Кнопка Классы отображается")
+    def test_button_grades_displayed(self):
+        self.assertTrue(self.is_element_present(By.XPATH, "//header/div[1]/div[1]"))
+
+    @allure.step("Элемент Поиск отображается")
+    def test_field_search(self):
+        self.assertTrue(self.is_element_present(By.CSS_SELECTOR, "div.header-search__wraps"))
+
+    @allure.step("Кнопка Флешка отображается")
+    def test_button_flash(self):
+        self.assertTrue(self.is_element_present(By.CSS_SELECTOR, "div.icon-flash"))
+
+    @allure.step("Кнопка Войти отображается")
+    def test_button_enter_is_displayed(self):
+        self.assertEqual(u"Войти", self.driver.find_element_by_css_selector("div.header-menu-wrapper").text)
