54c15c19b27892426ebd5d01e33f51fc0f3f3917
Mikhail-QA <Ponomarev_mi@mail.ru>
2018-03-19 18:00:41 +0300

    Web_services\URL
    Добавил класс class Paid_lesson_page(object):
    Ссылка на урок Алгебра 8 класс Основные понятния

--- Web_services/URL.py
+++ Web_services/URL.py
@@ -8,3 +8,11 @@ class SubjectPage(object):
     def go_literature_8_grade(self):
         self.driver.get("https://fast-staging.interneturok.ru/literatura/8-klass")
 
+
+class Paid_lesson_page(object):
+    def __init__(self, driver):
+        self.driver = driver
+
+    def go_lesson_page(self):
+        self.driver.get(
+            "https://fast-staging.interneturok.ru/algebra/8-klass/algebraicheskie-drobi-arifmeticheskie-operacii-nad-algebraicheskimi-drobyami/osnovnye-ponyatiya")
