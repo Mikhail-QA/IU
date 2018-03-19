e9ac43feb7c3d93cc406ae450abf521e87602c9f
Mikhail-QA <Ponomarev_mi@mail.ru>
2018-03-19 11:21:41 +0300

    Smoke\test_16_Пройти_тренажер_в_платном_уроке
    Поменял шаг перехода на страницу урока

--- Smoke/test_16_Пройти_тренажер_в_платном_уроке.py
+++ Smoke/test_16_Пройти_тренажер_в_платном_уроке.py
@@ -1,11 +1,11 @@
 import allure
-import time
 import unittest
 from POM.setUp import StartInterneturok
 from POM.main_page import MainPage
 from POM.popup_authorization_and_registration import PopupSignIn
 from POM.user import PaymNotYandexRu
 from POM.paid_exercise import Exercise
+from POM.url_lesson import URLPaidLesson
 
 
 @allure.feature("Тренажер")
@@ -17,6 +17,7 @@ class PassSimulatorInPayLesson(StartInterneturok):
         user_steps = PaymNotYandexRu(driver)
         popup_steps = PopupSignIn(driver)
         steps_exercise = Exercise(driver)
+        get_url = URLPaidLesson(driver)
         with allure.step("Нажать на кнопку Войти"):
             main_steps.go_to_sgnIn()
         with allure.step("Ввожу email/password"):
@@ -25,9 +26,7 @@ class PassSimulatorInPayLesson(StartInterneturok):
         with allure.step("Нажать на кнопку Авторизоваться"):
             popup_steps.click_button_login()
         with allure.step("Перейти на урок"):
-            self.driver.get(
-                "https://fast-staging.interneturok.ru/physics/7-klass/vzaimodejstvie-tel/plotnost/trainers")
-            time.sleep(2)
+            get_url.go_algebra_8_grade_trainers()
         with allure.step("Нажать на кнопку Пройти"):
             steps_exercise.go_exercise()
         with allure.step("Начать отвечать на ответы в Тренажере"):
