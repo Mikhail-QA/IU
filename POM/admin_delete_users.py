46bb68837604d56c3e62ffacbbbd1ec8810d6d14
Mikhail-QA <Ponomarev_mi@mail.ru>
2018-03-22 10:36:37 +0300

    POM\admin_delete_users
    Добавил ещё один тест на удаления П

--- POM/admin_delete_users.py
+++ POM/admin_delete_users.py
@@ -58,3 +58,15 @@ class AdminDeleteUser(object):
         time.sleep(1)
         self.driver.switch_to.alert.accept()
         time.sleep(6)
+
+    def user_5(self, pupil_4):
+        self.driver.find_element_by_id("search_name").clear()
+        self.driver.find_element_by_id("search_name").send_keys(pupil_4)
+        self.driver.find_element_by_css_selector("input.btn").click()
+        time.sleep(1)
+        self.driver.find_element_by_css_selector("div.btn-group").click()
+        time.sleep(1)
+        self.driver.find_element_by_link_text(u"Удалить").click()
+        time.sleep(1)
+        self.driver.switch_to.alert.accept()
+        time.sleep(6)
