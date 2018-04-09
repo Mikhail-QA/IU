5eca37967aaed7ff70b26af45d716d83c92d50fa
Mikhail <ponomarev@interneturok.ru>
2018-04-09 16:44:58 +0300

    Web_services\Main_page\Сheck_all_elements_on_the_home_page\test_попап_флешки
    Поменял индификаторы и добавил новые шаги

--- Web_services/Main_page/Сheck_all_elements_on_the_home_page/test_попап_флешки.py
+++ Web_services/Main_page/Сheck_all_elements_on_the_home_page/test_попап_флешки.py
@@ -10,7 +10,7 @@ URL: https://interneturok.ru/"	На странице отображаются: h
 URL:
 """
 
-import pytest
+import time
 import allure
 from selenium.webdriver.common.by import By
 from Web_services.SetUp import StartInterneturokClassMethod
@@ -22,7 +22,7 @@ from POM.user import PaymNotYandexRu
 
 @allure.feature("Главная страница")
 @allure.story("Проверка наличия элементов в виджете Флешка")
-@pytest.mark.skip(reason="Убрали Флешку из проекта")
+# @pytest.mark.skip(reason="Убрали Флешку из проекта")
 class ChecksAllElementsInPopupFlash(StartInterneturokClassMethod):
     @allure.step("Нажать на кнопку Флешка")
     def test_001_open_popup_flash(self):
@@ -30,35 +30,36 @@ class ChecksAllElementsInPopupFlash(StartInterneturokClassMethod):
 
     @allure.step("В поп-апе Флешка отображается кнопка Крестик(закрыть) ")
     def test_002_look_button_close(self):
-        self.assertTrue(self.is_element_present(By.LINK_TEXT, "close"))
+        self.assertTrue(self.is_element_present(By.CSS_SELECTOR, "button.button-close"))
 
     @allure.step("Отображается заголовок с текстом (Флешка InternetUrok.ru)")
     def test_003_text1(self):
         self.assertEqual(u"Флешка InternetUrok.ru",
-                         self.driver.find_element_by_css_selector("strong.sub-title._center-text").text)
+                         self.driver.find_element_by_css_selector("h3.title").text)
 
     @allure.step(
         "Отображается текст (Вы сможете загрузить уроки на свою флешку и заниматься без Интернета Подробнее об флешке)")
     def test_004_text2(self):
         self.assertEqual(
-            u"""Вы сможете загрузить уроки на свою флешку\nи заниматься без Интернета\nПодробнее об флешке""",
-            self.driver.find_element_by_css_selector("span.products-benefits__text").text)
+            u"""Вы можете загрузить уроки на свою флешку\nи заниматься без Интернета.\nПодробнее о Флешке""",
+            self.driver.find_element_by_css_selector(".__flash-info__b79b8 > div:nth-child(3) > div.text").text)
 
     @allure.step(
         "Отображается текст (Для загрузки уроков нужно войти в свою учетную запись и оплатить абонемент. Подробнее об абонементе")
     def test_005_text3(self):
         self.assertEqual(
-            u"""Для загрузки уроков нужно войти в свою\nучетную запись и оплатить абонемент.\nПодробнее об абонементе""",
-            self.driver.find_element_by_xpath("//div[@id='widget-wrapper']/div/div/div/ul/li[2]/span[2]").text)
+            u"""Для загрузки уроков нужно войти в свою учётную запись и оплатить абонемент. Подробнее об абонементе""",
+            self.driver.find_element_by_css_selector(".__flash-info__b79b8 > div:nth-child(4) > div.text").text)
 
     @allure.step("Отображается кнопка (Оплатить абонемент)")
     def test_006_look_button_pay(self):
-        self.assertTrue(self.is_element_present(By.LINK_TEXT, u"Оплатить абонемент"))
+        self.assertTrue(self.is_element_present(By.CSS_SELECTOR, "button.button-pay"))
 
     @allure.step("Отображается текст (У вас уже есть абонемент? Войти)")
     def test_007_text5(self):
-        self.assertEqual(u"У вас уже есть абонемент? Войти", self.driver.find_element_by_css_selector(
-            "#widget-wrapper > div > div > div > div.more-holder.text-center").text)
+        self.assertEqual(u"У Вас уже есть абонемент? Войти", self.driver.find_element_by_css_selector(
+            "p.subscribe-already").text)
+        self.driver.refresh()
 
     @allure.step("Авторизоваться пользователем без абонемента")
     def test_008_authorization_user(self):
@@ -72,37 +73,39 @@ class ChecksAllElementsInPopupFlash(StartInterneturokClassMethod):
         user_step.enter_password()
         steps_signIn.click_button_login()
         driver.refresh()
+        time.sleep(3)
 
     @allure.step("Нажать на кнопку Флешка")
     def test_009_open_popup_flash(self):
-        self.driver.find_element_by_css_selector("div.icon-flash").click()
+        self.driver.find_element_by_css_selector("i.icon-flash").click()
 
     @allure.step("В поп-апе Флешка отображается кнопка Крестик(закрыть) ")
     def test_10_look_button_close(self):
-        self.assertTrue(self.is_element_present(By.LINK_TEXT, "close"))
+        self.assertTrue(self.is_element_present(By.CSS_SELECTOR, "button.button-close"))
 
     @allure.step("Отображается заголовок с текстом (Флешка InternetUrok.ru)")
     def test_11_text1(self):
         self.assertEqual(u"Флешка InternetUrok.ru",
-                         self.driver.find_element_by_css_selector("strong.sub-title._center-text").text)
+                         self.driver.find_element_by_css_selector("h3.title").text)
 
     @allure.step(
-        "Отображается текст (Вы сможете загрузить уроки на свою флешку и заниматься без Интернета Подробнее об флешке)")
+        "Отображается текст (Вы можете загрузить уроки на свою флешку и заниматься без Интернета. Подробнее об флешке)")
     def test_12_text2(self):
         self.assertEqual(
-            u"""Вы сможете загрузить уроки на свою флешку\nи заниматься без Интернета\nПодробнее об флешке""",
-            self.driver.find_element_by_css_selector("span.products-benefits__text").text)
+            u"""Вы можете загрузить уроки на свою флешку\nи заниматься без Интернета.\nПодробнее о Флешке""",
+            self.driver.find_element_by_css_selector(".__flash-info__b79b8 > div:nth-child(3) > div.text").text)
 
     @allure.step(
         "Отображается текст (Для загрузки уроков нужно оплатить абонемент. Подробнее об абонементе)")
     def test_13_text3(self):
         self.assertEqual(
-            u"""Для загрузки уроков нужно оплатить\nабонемент. Подробнее об абонементе""",
-            self.driver.find_element_by_xpath("//div[@id='widget-wrapper']/div/div/div/ul/li[2]/span[2]").text)
+            u"""Для загрузки уроков нужно оплатить абонемент. Подробнее об абонементе""",
+            self.driver.find_element_by_css_selector(".__flash-info__b79b8 > div:nth-child(4) > div.text").text)
 
     @allure.step("Отображается кнопка (Оплатить абонемент)")
     def test_14_look_button_pay(self):
-        self.assertTrue(self.is_element_present(By.LINK_TEXT, u"Оплатить абонемент"))
+        self.assertTrue(self.is_element_present(By.CSS_SELECTOR, "button.button-pay"))
+        self.driver.refresh()
 
     @allure.step("Авторизоваться пользователем с абонементом")
     def test_15_authorization_user(self):
@@ -111,34 +114,40 @@ class ChecksAllElementsInPopupFlash(StartInterneturokClassMethod):
         user_step = PaymNotYandexRu(driver)
         steps_signIn = PopupSignIn(driver)
 
-        driver.get("https://fast-staging.interneturok.ru/")
+        # driver.get("https://staging.interneturok.ru/")
+        self.driver.delete_all_cookies()
+        driver.refresh()
         steps_main.go_to_sgnIn()
         user_step.enter_email()
         user_step.enter_password()
         steps_signIn.click_button_login()
-        driver.refresh()
 
     @allure.step("Нажать на кнопку Флешка")
     def test_16_open_popup_flash(self):
-        self.driver.find_element_by_css_selector("div.icon-flash").click()
+        self.driver.find_element_by_css_selector("i.icon-flash").click()
 
     @allure.step("В поп-апе Флешка отображается кнопка Крестик(закрыть) ")
     def test_17_look_button_close(self):
-        self.assertTrue(self.is_element_present(By.LINK_TEXT, "close"))
+        self.assertTrue(self.is_element_present(By.CSS_SELECTOR, "button.button-close"))
 
     @allure.step("Отображается текст (Доступные для загрузки наборы уроков:)")
     def test_18_text1(self):
         self.assertEqual(u"Доступные для загрузки наборы уроков:",
                          self.driver.find_element_by_css_selector(
-                             "#widget-availables-list > strong").text)
+                             "h3.__flash-available-lessons__2288e-title").text)
+
+    @allure.step("Отображается список бандлов к загрузке")
+    def test_19_look_board_bandl(self):
+        self.assertTrue(self.is_element_present(By.CSS_SELECTOR, "div.lessons-border"))
 
     @allure.step(
         "Отображается текст (Ваш список загрузки На флешке нет уроков. Добавляйте уроки со страниц InternetUrok.ru и занимайтесь без интернета Подробнее о флешке)")
-    def test_19_text2(self):
+    def test_20_text2(self):
         self.assertEqual(
-            u"Ваш список загрузки\nНа флешке нет уроков. Добавляйте уроки со страниц\nInternetUrok.ru и занимайтесь без интернета\nПодробнее о флешке",
-            self.driver.find_element_by_css_selector("#widget-wrapper > div > div:nth-child(2)").text)
+            u"Ваш список загрузки\nНа флешке нет уроков. Добавляйте уроки со страниц InternetUrok.ru и занимайтесь без Интернета\nПодробнее о Флешке",
+            self.driver.find_element_by_css_selector("div.__flash-download-lessons__576bd").text)
 
     @allure.step("Отображается текст (Подробнее о флешке)")
-    def test_20_look_button_more_about_the_flash(self):
-        self.assertTrue(self.is_element_present(By.LINK_TEXT, u"Подробнее о флешке"))
+    def test_21_look_button_more_about_the_flash(self):
+        self.assertEqual("Подробнее о флешке",
+                         self.driver.find_element_by_css_selector("a.button-about-flash").text)
