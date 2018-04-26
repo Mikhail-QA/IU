8d118d30c847630b1035c71e048a5c484cd5a6d4
Mikhail <ponomarev@interneturok.ru>
2018-04-26 12:23:16 +0300

    Smoke\test_22_Проверить_видео_в_плеере_ИУ
    Изменил описание декоратора allure.story и добавил в декоратор название урока в который переходит П

--- Smoke/test_22_Проверить_видео_в_плеере_ИУ.py
+++ Smoke/test_22_Проверить_видео_в_плеере_ИУ.py
@@ -6,13 +6,13 @@ from POM.url_lesson import URLFreeLesson
 
 
 @allure.feature("Плеер ИУ")
-@allure.story("Проверяю воспроизведения видео в плеере ИУ")
+@allure.story("Проверить воспроизведения видео в плеере ИУ")
 class PlayVideoIu(StartInterneturok):
     def test_check_player_iu(self):
         driver = self.driver
         steps_video = PageFreeLessonCheckVideo(driver)
         get_url = URLFreeLesson(driver)
-        with allure.step("Перейти на урок"):
+        with allure.step("Перейти на урок =Обзор эволюционных представлений"):
             get_url.go_biology_11_grade_video()
         with allure.step("Нажать на кнопку Плей"):
             steps_video.click_play_video_iu()
