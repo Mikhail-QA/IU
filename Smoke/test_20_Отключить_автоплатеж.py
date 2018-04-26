12581c82be49b4fdb56febeb397b002505f102c0
Mikhail <ponomarev@interneturok.ru>
2018-04-26 12:17:42 +0300

    Smoke\test_20_Отключить_автоплатеж
    Добавил лог и пасс в декоратор

--- Smoke/test_20_Отключить_автоплатеж.py
+++ Smoke/test_20_Отключить_автоплатеж.py
@@ -19,17 +19,15 @@ class DisableAutoPayment(StartInterneturok):
         profile_steps = PageProfile(driver)
         with allure.step("Нажать на кнопку Войти"):
             main_steps.go_to_sgnIn()
-        with allure.step("Ввожу email/password"):
+        with allure.step("Ввожу email=vratch.glav@yandex.ru/password=123456"):
             user_steps.enter_email()
             user_steps.enter_password()
         with allure.step("Нажать на кнопку Авторизоваться"):
             popup_steps.click_button_login()
         with allure.step("Перейти в ЛК"):
             profile_steps.go_to_my_profile()
-            time.sleep(1)
         with allure.step("В виджете абонемент нажать на кнопку Выкл автоплатёж"):
             profile_steps.click_button_off_autopayment()
-            time.sleep(1.5)
         with allure.step("В появившемся тултипе нажать на кнопку Отключить автопродление"):
             profile_steps.click_in_popup1_off_autopayment()
         with allure.step("В появившемся поп-апе нажать на кнопку Отключить автопродление"):
