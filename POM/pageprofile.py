54ca9236296e8d63b7b68fc6c5ed84f0fde0388c
Mikhail <ponomarev@interneturok.ru>
2018-04-26 11:14:36 +0300

    POM\pageprofile
    Добавил класс time.sleep(1)

--- POM/pageprofile.py
+++ POM/pageprofile.py
@@ -46,6 +46,7 @@ class PageProfile(object):
 
     def go_to_my_profile(self):
         self.driver.get("https://staging.interneturok.ru/profile")
+        time.sleep(1)
 
     def go_to_my_profile_edit(self):
         self.driver.get("https://staging.interneturok.ru/profile/edit")
