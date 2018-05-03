0b8b7b16149032fc106070e3b8c860c0009b7446
Mikhail <ponomarev@interneturok.ru>
2018-05-03 09:36:12 +0300

    Web_services\URL.
      Поменял URL

--- Web_services/URL.py
+++ Web_services/URL.py
@@ -6,11 +6,11 @@ class SubjectPage(object):
         self.driver = driver
 
     def go_algebra_8_grade(self):
-        self.driver.get("https://staging.interneturok.ru/algebra/8-klass")
+        self.driver.get("https://staging.interneturok.ru/predmet/algebra/klass/8/8")
         time.sleep(1)
 
     def go_literature_8_grade(self):
-        self.driver.get("https://staging.interneturok.ru/literatura/8-klass")
+        self.driver.get("https://staging.interneturok.ru/predmet/literatura/klass/8/96")
 
 
 class Paid_lesson_page(object):
@@ -19,20 +19,20 @@ class Paid_lesson_page(object):
 
     def go_lesson_page(self):
         self.driver.get(
-            "https://staging.interneturok.ru/algebra/8-klass/algebraicheskie-drobi-arifmeticheskie-operacii-nad-algebraicheskimi-drobyami/osnovnye-ponyatiya")
+            "https://staging.interneturok.ru/urok/algebraicheskie-drobi-arifmeticheskie-operacii-nad-algebraicheskimi-drobyamiosnovnye-ponyatiya/75")
         time.sleep(1)
 
     def go_lesson_page_tab_trainers(self):
         self.driver.get(
-            "https://staging.interneturok.ru/algebra/8-klass/algebraicheskie-drobi-arifmeticheskie-operacii-nad-algebraicheskimi-drobyami/osnovnye-ponyatiya/trainers")
+            "https://staging.interneturok.ru/urok/algebraicheskie-drobi-arifmeticheskie-operacii-nad-algebraicheskimi-drobyamiosnovnye-ponyatiya/75/trainers")
         time.sleep(1)
 
     def go_lesson_page_tab_test(self):
         self.driver.get(
-            "https://staging.interneturok.ru/algebra/8-klass/algebraicheskie-drobi-arifmeticheskie-operacii-nad-algebraicheskimi-drobyami/osnovnye-ponyatiya/testcases")
+            "https://staging.interneturok.ru/urok/algebraicheskie-drobi-arifmeticheskie-operacii-nad-algebraicheskimi-drobyamiosnovnye-ponyatiya/75/testcases")
         time.sleep(1)
 
     def go_lesson_page_tab_question(self):
         self.driver.get(
-            "https://staging.interneturok.ru/algebra/8-klass/algebraicheskie-drobi-arifmeticheskie-operacii-nad-algebraicheskimi-drobyami/osnovnye-ponyatiya/questions")
+            "https://staging.interneturok.ru/urok/algebraicheskie-drobi-arifmeticheskie-operacii-nad-algebraicheskimi-drobyamiosnovnye-ponyatiya/75/questions")
         time.sleep(1)
