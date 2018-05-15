16943b5cce266d360297292c73c80c47b6d0db11
Mikhail <ponomarev@interneturok.ru>
2018-05-15 15:12:15 +0300

    POM\page_search
    Исправил ошибки

--- POM/page_search.py
+++ POM/page_search.py
@@ -3,4 +3,4 @@ class Search(object):
         self.driver = driver
 
     def test_click_to_video(self):
-        self.driver.find_elements.by_css_selector(".ember-view > div.search-result__box_item-thumb").click()
+        self.driver.find_element_by_css_selector(".ember-view > div.teacher-tabs__box_item-content > p").click()
