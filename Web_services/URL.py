40a81901f67a05bbdee7a4baeda26754c95f472b
Mikhail <ponomarev@interneturok.ru>
2018-05-21 13:52:15 +0300

    Web_services\URL
    Поменял ссылки на уроки

--- Web_services/URL.py
+++ Web_services/URL.py
@@ -6,11 +6,11 @@ class SubjectPage(object):
         self.driver = driver
 
     def go_algebra_8_grade(self):
-        self.driver.get("https://staging.interneturok.ru/algebra/8-klass")
+        self.driver.get("https://staging.interneturok.ru/subject/algebra/class/8")
         time.sleep(1)
 
     def go_literature_8_grade(self):
-        self.driver.get("https://staging.interneturok.ru/literatura/8-klass")
+        self.driver.get("https://staging.interneturok.ru/subject/literatura/class/8")
 
 
 class PaidLessonPage(object):
@@ -19,21 +19,20 @@ class PaidLessonPage(object):
 
     def go_lesson_page(self):
         self.driver.get(
-            "https://staging.interneturok.ru/algebra/10-klass/trigonometricheskie-funkcii/periodichnost-funktsiy-y-sin-t-y-cos-t")
+            "https://staging.interneturok.ru/lesson/algebra/10-klass/trigonometricheskie-funkcii/periodichnost-funktsiy-y-sin-t-y-cos-t")
         time.sleep(1)
 
     def go_lesson_page_tab_trainers(self):
         self.driver.get(
-            "https://staging.interneturok.ru/algebra/10-klass/trigonometricheskie-funkcii/periodichnost-funktsiy-y-sin-t-y-cos-t/trainers")
+            "https://staging.interneturok.ru/lesson/algebra/10-klass/trigonometricheskie-funkcii/periodichnost-funktsiy-y-sin-t-y-cos-t/trainers")
         time.sleep(1)
 
     def go_lesson_page_tab_test(self):
         self.driver.get(
-            "https://staging.interneturok.ru/algebra/10-klass/trigonometricheskie-funkcii/periodichnost-funktsiy-y-sin-t-y-cos-t/testcases")
+            "https://staging.interneturok.ru/lesson/algebra/10-klass/trigonometricheskie-funkcii/periodichnost-funktsiy-y-sin-t-y-cos-t/testcases")
         time.sleep(1)
 
     def go_lesson_page_tab_question(self):
         self.driver.get(
-            "https://staging.interneturok.ru/algebra/10-klass/trigonometricheskie-funkcii/periodichnost-funktsiy-y-sin-t-y-cos-t/questions"
-            "")
+            "https://staging.interneturok.ru/lesson/algebra/10-klass/trigonometricheskie-funkcii/periodichnost-funktsiy-y-sin-t-y-cos-t/questions")
         time.sleep(1)
