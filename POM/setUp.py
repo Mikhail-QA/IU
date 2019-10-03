ef3cf5252492948abd7da9fea57eb7b06ce9f728
Mikhail <ponomarev@interneturok.ru>
2019-10-03 16:52:18 +0500

    Поменял URL и создал новый класс StartInterneturokLendingClassMethod

--- POM/setUp.py
+++ POM/setUp.py
@@ -9,7 +9,7 @@ class StartInterneturok(unittest.TestCase):
         self.driver = webdriver.Chrome()
         self.driver.implicitly_wait(40)
         self.driver.maximize_window()
-        self.driver.get("https://staging.interneturok.ru")
+        self.driver.get("https://staging.interneturok.ru/class")
         time.sleep(3)
         self.verificationErrors = []
 
@@ -61,6 +61,34 @@ class StartInterneturokClassMethod(unittest.TestCase):
         cls.driver.implicitly_wait(40)
         cls.driver.maximize_window()
         cls.driver.get("https://staging.interneturok.ru/class")
+        time.sleep(3)
+        cls.verificationErrors = []
+
+    def is_element_present(self, how, what):
+        try:
+            self.driver.find_element(by=how, value=what)
+        except NoSuchElementException:
+            return False
+        return True
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.quit()
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+
+class StartInterneturokLendingClassMethod(unittest.TestCase):
+    driver = None
+
+    @classmethod
+    def setUpClass(cls):
+        cls.driver = webdriver.Chrome()
+        cls.driver.implicitly_wait(40)
+        cls.driver.maximize_window()
+        cls.driver.get("https://staging.interneturok.ru")
         time.sleep(3)
         cls.verificationErrors = []
 
